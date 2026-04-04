3d86b2dd49bedee8d3a8be9d69a62cbab6f6b0cc
SnowAgent67 <ramireznu666@gmail.com>
2026-04-04 16:25:50 -0600

    Interfaz en celo

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# SCRIPT PRINCIPAL
-# Este script simplemente es el que se debe ejecutar para que la aplicación
-# inicie. Este se encarga de ejecutar la página principal, app.py.
-
+import flet as ft
+#from App import main
 from src.ui.pages.app import main
 
 if __name__ == "__main__":
     main()
+    #ft.app(target=main)
